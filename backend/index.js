32a3149e9b3d52bea52251fae03ef9ac46e47e34
Nilophar786 <Shaikhnilofar0027@gmail.com>
2026-07-11 01:07:17 +0530

    Update allowed origins for CORS

--- backend/index.js
+++ backend/index.js
@@ -21,8 +21,7 @@ app.use(cors({
         if (!origin) return callback(null, true);
 
         const allowedOrigins = [
-            "http://localhost:5173",
-            "http://localhost:5174",
+            "https://advanced-voice-assistant-1.onrender.com",
            
         ];
 
